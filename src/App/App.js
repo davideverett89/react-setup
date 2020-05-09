0a0917111c6ad6f2f1134abd005d527badc658d8
David Everett <davideverett1989@gmail.com>
2020-05-09 11:14:33 -0500

    Setup complete.

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>HELLO WORLD!</h1>
+        <button className="btn btn-dark">Click Me!</button>
+      </div>
+    );
+  }
+}
+
+export default App;
